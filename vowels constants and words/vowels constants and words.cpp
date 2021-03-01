24e7b7ec2ef266712c7b8c928b704197f0d82a33
rarelambo21 <35502394+rarelambo21@users.noreply.github.com>
2021-03-01 11:45:18 +0300

    Vowels Constants and Words

--- vowels constants and words/vowels constants and words.cpp
+++ vowels constants and words/vowels constants and words.cpp
@@ -2,10 +2,62 @@
 //
 
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string str;
+
+    int vowels = 0, constants = 0, words = 1;
+
+    str = "how Many wOrds";
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ' ')
+            words++;
+        else {
+            switch (str[i]){
+            case 'A':
+                vowels++;
+                break;
+            case 'a':
+                vowels++;
+                break;
+            case 'E':
+                vowels++;
+                break;
+            case 'e':
+                vowels++;
+                break;
+            case 'I':
+                vowels++;
+                break;
+            case 'i':
+                vowels++;
+                break;
+            case 'O':
+                vowels++;
+                break;
+            case 'o':
+                vowels++;
+                break;
+            case 'U':
+                vowels++;
+                break;
+            case 'u':
+                vowels++;
+                break;
+            default:
+                constants++;
+                break;
+            }
+        }
+    }
+    std::cout << "Words: " << words << std::endl;
+    std::cout << "Vowels: " << vowels << std::endl;
+    std::cout << "Constants: " << constants << std::endl;
+
+    return 0;
 }
 
 
